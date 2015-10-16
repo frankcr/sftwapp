a6c686505a683e8650925f44e9d82e568e304379
frankcr <frankcrijns@outlook.com>
2015-10-16 22:19:06 +0200

    Working version with angular material

--- www/js/app.js
+++ www/js/app.js
@@ -1,12 +1,12 @@
 angular.module('sftw', ['ionic', 'sftw.controllers', 'sftw.directives', 'sftw.services', 'sftw.resources', 'ngMaterial'])
-  .constant('ApiEndpoint', {
-    url: 'http://localhost:8100/api'
-  })
+  //.constant('ApiEndpoint', {
+  //  url: 'http://localhost:8100/api'
+  //})
 
-   //For the real endpoint, we'd use this
-   //.constant('ApiEndpoint', {
-   // url: 'http://sftw.be'
-   //})
+  //For the real endpoint, we'd use this
+  .constant('ApiEndpoint', {
+   url: 'http://sftw.be'
+  })
 
   .run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
@@ -14,55 +14,56 @@ angular.module('sftw', ['ionic', 'sftw.controllers', 'sftw.directives', 'sftw.se
         StatusBar.styleDefault();
       }
 
-      if(window.Connection) {
-        if(navigator.connection.type == Connection.NONE) {
+      if (window.Connection) {
+        if (navigator.connection.type == Connection.NONE) {
           $ionicPopup.confirm({
             title: "Internet Disconnected",
             content: "The internet is disconnected on your device."
           })
-            .then(function(result) {
-              if(!result) {
+            .then(function (result) {
+              if (!result) {
                 ionic.Platform.exitApp();
               }
             });
         }
       }
     });
-  }).config(function($stateProvider, $urlRouterProvider) {
+
+  }).config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
 
       .state('app', {
         url: "/app",
         abstract: true,
-        templateUrl: "../menu.html",
+        templateUrl: "menu.html",
         controller: 'AppCtrl'
       })
 
       .state('app.map', {
         url: "/map",
         views: {
-          'menuContent' :{
-            templateUrl: "../map.html",
+          'menuContent': {
+            templateUrl: "map.html",
             controller: 'MapCtrl'
           }
         }
       }).state('app.photography', {
         url: "/photography",
         views: {
-          'menuContent' :{
-            templateUrl: "../photography.html",
+          'menuContent': {
+            templateUrl: "photography.html",
             controller: 'PhotographyCtrl'
           }
         }
       }).state('app.settings', {
         url: "/settings",
         views: {
-          'menuContent' :{
-            templateUrl: "../settings.html",
+          'menuContent': {
+            templateUrl: "settings.html",
             controller: 'SettingsCtrl'
           }
         }
       });
-    // if none of the above states are matched, use this as the fallback
+    //   if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('/app/map');
   });
